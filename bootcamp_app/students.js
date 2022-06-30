a8416ae8140eae5e4b71d718d2556cf7cdd1bc77
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-30 01:30:04 -0600

    Node Postgres 2022-0630-0130

---
+++ bootcamp_app/students.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const cohort_name = process.argv[2];
+const cohort_limit = process.argv[3];
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+ LIMIT ${cohort_limit};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${cohort_name} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,45 @@
+const { LOADIPHLPAPI } = require('dns');
+const { Pool } = require('pg');
+
+//SHENG
+// const cohort_name = process.argv[2];
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.query(`
+//   SELECT teachers.name
+//     FROM teachers
+//     JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+//     JOIN students ON students.id = assistance_requests.student_id
+//     JOIN cohorts ON cohorts.id = students.cohort_id
+//    WHERE cohorts.name = ${cohort_name}
+// GROUP BY teachers.name
+// ORDER BY teachers.name;
+//   `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${cohort_name}: ${user.name}`);
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
+
+// LHL
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
