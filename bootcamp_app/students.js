1aca6136a0a6a10942931ad7d3caf481a70d845d
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-30 09:19:06 -0600

    inject 2022-0630-0919

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 
 const cohort_name = process.argv[2];
 const cohort_limit = process.argv[3];
+const values = [`${cohort_name}`, `${cohort_limit}`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -14,8 +15,8 @@ pool.query(`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
   FROM students
   JOIN cohorts ON cohorts.id = students.cohort_id
- LIMIT ${cohort_limit};
-`)
+ LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${cohort_name} cohort`);
